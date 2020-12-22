f83043cf1ee2085f3ff111fd4c1880b84258b191
Given Kafuwa <gkafuwa@gmail.com>
2020-12-22 15:58:22 +0200

    removed extra code. Added example project

--- build.js
+++ build.js
@@ -4,22 +4,21 @@ var electronInstaller = require('electron-winstaller');
 // In this case, we can use relative paths
 var settings = {
     // Specify the folder where the built app is located
-    appDirectory: './app-win32-x64',
+    appDirectory: './timer-win32-x64',
     // Specify the existing folder where 
     outputDirectory: './app_installer',
     // The name of the Author of the app (the name of your company)
     authors: 'Giv',
     // The name of the executable of your built
-    exe: './app.exe',
-    // setupIcon: `file://${__dirname}/kaizen.icon`,
-    // setupIcon: './dist/assets/kaizen.ico',
-    setupIcon: './icons/icon.ico',
-    iconUrl: `http://test.padziwe.org/assets/images/kaizen.ico`,
+    exe: './timer.exe',
+    // setupIcon: `file://${__dirname}/icons/icon.ico`,
+    // setupIcon: './icons/icon.ico',
+    // iconUrl: `http://localhost:8080/images/icon.ico`,
     // 👆 in most cases it only accepts web url and not local file. you might want to serve it over an http server 
-    // iconUrl: `./kaizen.ico`,
-    setupExe: 'kaizen.exe',
+    // iconUrl: `./icons/icon.ico`,
+    setupExe: 'timer.exe',
     noMsi: true,
-    loadingGif: './icons/loader.gif'
+    // loadingGif: './icons/loader.gif'
     // 👆 this is the Gif that will appear when app is loading
 };
 
